a0bb6846bd5b85f8b5d85d1455c805b8bf3c2972
SohamBhattacharya <soham.elessar@gmail.com>
2025-02-14 15:51:01 -0800

    Minor fixes

--- scripts/archive_dir.sh
+++ scripts/archive_dir.sh
@@ -12,17 +12,19 @@ fi
 DIRPATH=`realpath $DIRPATH`
 DIRNAME=`basename $DIRPATH`
 PARENT=`dirname $DIRPATH`
-OUTPUT="$DIRNAME.tar"
+OUTPUT="${DIRNAME}.tar"
 
 cd $PARENT
 
 if [ ! -f $OUTPUT ]; then
     echo "Creating $OUTPUT"
-    tar cfv $OUTPUT $DIRNAME
+    #tar cfv $OUTPUT $DIRNAME
+    tar -cv --exclude "*.pdf" -f "${OUTPUT}" "${DIRNAME}"
 else
     echo "Updating $OUTPUT"
     # The u flag will only update the tar file with new content
-    tar ufv $OUTPUT $DIRNAME
+    #tar ufv $OUTPUT $DIRNAME
+    tar -cuv -exclude *.pdf -f "${OUTPUT}" "${DIRNAME}"
 fi
 
 
