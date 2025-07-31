e3c681fed64747acde798d381727cac6b6d4f38c
SohamBhattacharya <soham.elessar@gmail.com>
2025-07-31 15:20:36 -0700

    Minor fix

--- scripts/CIT/mount_cptlab_share.sh
+++ scripts/CIT/mount_cptlab_share.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup rclone mount cptlab_share:/disk1/share ~/mnt/cptlab_share --vfs-cache-mode full --rc --rc-no-auth -vv >>/dev/null 2>&1 &
+nohup rclone mount cptlab_share:/disk1/share ~/mnt/cptlab_share --vfs-cache-mode full --rc --rc-no-auth -vv > /dev/null &
